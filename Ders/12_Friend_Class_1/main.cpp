908bfb0510d07c805d7d8ed30f97d8cdbb2d164e
Hüseyin YÜCE <huseyin@marmara.edu.tr>
2024-12-03 12:16:53 +0300

    Update main.cpp
    ufak değişilklikler

--- Ders/12_Friend_Class_1/main.cpp
+++ Ders/12_Friend_Class_1/main.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
-
 using namespace std;
-
 class Sinif_B;
-
-class Sinif_A{
-public:
-    Sinif_A():a(10){};
-   friend class Sinif_B;
+class Sinif_A {
 private:
     int a;
-};
-
-class Sinif_B{
 public:
-    void getGoster_A(Sinif_A &x);
-
+    Sinif_A():a(10){};
+    friend class Sinif_B;
+};
+class Sinif_B {
 private:
     int b;
+public:
+    void getDeger(Sinif_A &sinif_a);
+
 };
-
-void Sinif_B::getGoster_A(Sinif_A &x){
-    cout << "Sinif_A::a=" << x.a << endl;
+void Sinif_B::getDeger(Sinif_A &sinif_a) {
+    cout<<"Sinif_A::a= "<<sinif_a.a<<endl;
 }
-
 
 int main() {
     Sinif_A Nesne_A;
     Sinif_B Nesne_B;
-    Nesne_B.getGoster_A(Nesne_A);
+    Nesne_B.getDeger(Nesne_A);
+
 
     return 0;
 }
