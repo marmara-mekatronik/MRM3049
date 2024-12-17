db29c164a8a22348c2755e4d508883b8bb11be1a
Hüseyin YÜCE <huseyin@marmara.edu.tr>
2024-12-17 09:42:34 +0300

    Update main.cpp

--- Ders/19_statik_uye/main.cpp
+++ Ders/19_statik_uye/main.cpp
@@ -1,43 +1,59 @@
 #include <iostream>
-
 using namespace std;
-
 class Kutu {
-public:
-    static int nesneSayisi;
-
-    Kutu(double g = 2.0, double d = 2.0, double y = 2.0);
-
-    double hacim();
-
 private:
-    double genislik;
-    double derinlik;
-    double yukseklik;
+    double genislik, derinlik, yukseklik;
+    public:
+    static int nesneSayisi;
+    Kutu(double g=2.0, double d=2.0, double y=2.0);
+    double getHacim();
 };
-
 Kutu::Kutu(double g, double d, double y) {
-    cout << "Yapici Fanksiyon Yükleniyor...." << endl;
     genislik = g;
     derinlik = d;
     yukseklik = y;
-
     nesneSayisi++;
-    //^^^^^^^^^^^
 }
-
-double Kutu::hacim() {
+double Kutu::getHacim() {
     return genislik * derinlik * yukseklik;
 }
-
-int Kutu::nesneSayisi=0;
-
+int Kutu::nesneSayisi = 0;
 int main() {
-    Kutu Kutu1(3.3,1.2,2.3);
-    Kutu Kutu2(8.1,8.2,8.3);
+    Kutu Kutu1(3.3,4.4,5.5);
+    Kutu Kutu2(6.3,8.4,10.5);
 
     cout << "Oluşturulan Toplam Nesne Sayısı :" << Kutu::nesneSayisi << endl;
-    cout << "Kutu 1'deki değer : " << Kutu1.nesneSayisi << endl;
-    cout << "Kutu 2'deki değer : " << Kutu2.nesneSayisi << endl;
+    cout << "Kutu 1 hacmi : " << Kutu1.getHacim() << endl;
+    cout << "Kutu 2 hacim : " << Kutu2.getHacim() << endl;
+
+    return 0;
+}#include <iostream>
+using namespace std;
+class Kutu {
+private:
+    double genislik, derinlik, yukseklik;
+    public:
+    static int nesneSayisi;
+    Kutu(double g=2.0, double d=2.0, double y=2.0);
+    double getHacim();
+};
+Kutu::Kutu(double g, double d, double y) {
+    genislik = g;
+    derinlik = d;
+    yukseklik = y;
+    nesneSayisi++;
+}
+double Kutu::getHacim() {
+    return genislik * derinlik * yukseklik;
+}
+int Kutu::nesneSayisi = 0;
+int main() {
+    Kutu Kutu1(3.3,4.4,5.5);
+    Kutu Kutu2(6.3,8.4,10.5);
+
+    cout << "Oluşturulan Toplam Nesne Sayısı :" << Kutu::nesneSayisi << endl;
+    cout << "Kutu 1 hacmi : " << Kutu1.getHacim() << endl;
+    cout << "Kutu 2 hacim : " << Kutu2.getHacim() << endl;
+
     return 0;
 }
