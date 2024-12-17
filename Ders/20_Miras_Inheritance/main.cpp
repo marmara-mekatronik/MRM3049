c640906f52050a024e85aa04738128e2f6b570d6
Hüseyin YÜCE <huseyin@marmara.edu.tr>
2024-12-17 11:22:50 +0300

    Update main.cpp

--- Ders/20_Miras_Inheritance/main.cpp
+++ Ders/20_Miras_Inheritance/main.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
 using namespace std;
-class Sekil{
-public:
-    Sekil(int g=1, int y=1);
-    void setYaz(int g, int y);
-    void getOku();
-private:
+
+class Sekil {
 protected:
     int genislik, yukseklik;
-};
-Sekil::Sekil(int g, int y) {
-    genislik=g; yukseklik=y;
-}
-void Sekil::setYaz(int g, int y) {
-    genislik=g; yukseklik=y;
-}
-void Sekil::getOku() {
-    cout << "Nesnesinin Genisligi : " << genislik << endl;
-    cout << "Nesnesinin Yuksekligi : " << yukseklik << endl;
-}
-class Diktorgen: public Sekil{
 public:
-    Diktorgen(int g=1, int y=1);
-    int getAlan();
-private:
-protected:
+    Sekil(int gen, int yuk):genislik(gen),yukseklik(yuk) {}
+    void setYaz(int gen, int yuk){
+        genislik=gen;
+        yukseklik=yuk;
+    }
+    void getOku() {
+        cout << "Nesnenin Genisligi : " << genislik << endl;
+        cout << "Nesnenin Yuksekligi : " << yukseklik << endl;
+    }
 };
-Diktorgen::Diktorgen(int g, int y) {
-    genislik=g;yukseklik=y;
-}
-int Diktorgen::getAlan() {
-    return genislik*yukseklik;
-}
-int main() {
-    Diktorgen D1;
-    D1.getOku();
-    cout << "D1 nesnesinin alani: "<< D1.getAlan()<< endl;
-    D1.setYaz(20,50);
-    D1.getOku();
-    cout << "D1 nesnesinin alani: "<< D1.getAlan()<< endl;
+class Dikdortgen : public Sekil {
+public:
+    Dikdortgen(int gen, int yuk) : Sekil(gen, yuk) {}
+    int getAlan() {
+        return genislik * yukseklik;
+    }
+};
+int main()
+{
+    Dikdortgen *D1=new Dikdortgen(3,4);
+    D1->getOku();
+    cout << "Diktortgenin Alani: " << D1->getAlan() << endl;
+    D1->setYaz(10,20);
+    D1->getOku();
+    cout << "Diktortgenin Alani: " << D1->getAlan() << endl;
+    delete D1;
     return 0;
 }
